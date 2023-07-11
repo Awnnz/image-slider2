44fb7c90ed841d63cc6a175e604f92bfb29048b2
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-07-11 02:14:49 -0700

    template for future webpack projects

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+      ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Output Management',
+        template: './src/index.html'
+      }),
+  ]
+};
+
+module.exports = config;
